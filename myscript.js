6cf6c03a9f76095c753999121fab1ff095b7436b
lahcenrahlaoui <59196936+lahcenrahlaoui@users.noreply.github.com>
2025-06-20 21:07:12 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,3 +1,17 @@
-setInterval(() => {
-  console.log("Hello from my CDN script!");
-}, 5000);
+(function(){
+  const _encoded = [
+    "bG9jYWxob3N0",           // "localhost"
+    "ZXhhbXBsZS5jb20=",       // "example.com"
+    "YW5vdGhlcmRvbWFpbi5jb20=" // "anotherdomain.com"
+  ];
+  const _allowed = _encoded.map(atob);
+  const _host = location.hostname;
+
+  if (_allowed.includes(_host)) {
+    setInterval(() => {
+      console.log("Hello from my CDN script!");
+    }, 5000);
+  } else {
+    console.warn("Blocked by CDN script on:", _host);
+  }
+})();
